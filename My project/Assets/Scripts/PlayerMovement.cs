bb29619e99680851794921e51ed2557822589bdc
alhashij <alhashij@sheridancollege.ca>
2023-03-07 12:19:19 -0500

    Player Movement

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,32 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-    
+
+    private Vector2 moveDirection;
+
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Inputs();
     }
 
     private void FixedUpdate()
     {
-        
+
     }
+    void Inputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+
 }
